17f505f7ba43dd70a1854b118a5ddc35d65dd640
Christina33810 <luckyaroundme123@gmail.com>
2025-12-16 22:07:03 +0800

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Retail Business Dashboard")
+st.header("Manager Input Section")
+st.write("Please enter the monthly sales target and select the region")
+
+monthly_sales = st.number_input("Enter Monthly Sales Target (in USD):")
+
+region = st.selectbox("Select region:",["North", "South", "East", "West"])
+
+if st.button("Submit"):
+  st.success("Dashboard updated successfully!")
+  st.write(f"The {region} monthly sales target is {monthly_sales} in USD.")
+
+  if monthly_sales > 1000000:
+    st.write("Great! You have set an ambitious target!")
+                                
